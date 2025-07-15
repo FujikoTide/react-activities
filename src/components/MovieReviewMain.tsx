ef0499045eceba5d273b558d82d327d846080f9a
Robin Mukherjee <robin@otaking.co.uk>
2025-07-15 17:28:29 +0100

    add show review functionality

--- src/components/MovieReviewMain.tsx
+++ src/components/MovieReviewMain.tsx
@@ -19,12 +19,28 @@ interface FormDataState {
   rating: number
 }
 
+interface ReviewState {
+  id: number
+  show: boolean
+}
+
+const getReviewCount = (reviewData: ReviewDataState[], movieId: number) => {
+  const reviewCount = reviewData.filter(
+    (review) => review.movieId === movieId,
+  ).length
+  return reviewCount > 0 ? `Reviews (${reviewCount})` : 'No Reviews'
+}
+
 export default function MovieReviewMain({
   movieData,
   reviewData,
   setReviewData,
 }: MovieReviewMainProps) {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [showReview, setShowReview] = useState<ReviewState>({
+    id: 0,
+    show: false,
+  })
   const [modalData, setModalData] = useState<MovieDataItem | null>(null)
   const { id, setId, incrementID } = useId(0)
   const [formData, setFormData] = useState<FormDataState>({
@@ -43,6 +59,15 @@ export default function MovieReviewMain({
     }
   }, [reviewData, id, setId])
 
+  useEffect(() => {
+    if (modalData?.id) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        movieId: modalData.id,
+      }))
+    }
+  }, [modalData])
+
   const handleOpenModal = (data: MovieDataItem) => {
     setIsModalOpen(true)
     setModalData(data)
@@ -50,6 +75,21 @@ export default function MovieReviewMain({
 
   const handleCloseModal = () => {
     setIsModalOpen(false)
+  }
+
+  const handleReview = (movie: MovieDataItem) => {
+    if (movie.id === showReview.id) {
+      setShowReview((prevReviewData) => ({
+        ...prevReviewData,
+        show: !showReview.show,
+      }))
+    } else {
+      setShowReview((prevReviewData) => ({
+        ...prevReviewData,
+        id: movie.id,
+        show: true,
+      }))
+    }
   }
 
   const handleForm = (
@@ -61,15 +101,6 @@ export default function MovieReviewMain({
       [name]: type === 'number' ? Number(value) : value,
     }))
   }
-
-  useEffect(() => {
-    if (modalData?.id) {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        movieId: modalData.id,
-      }))
-    }
-  }, [modalData])
 
   const submitReview = (e: React.FormEvent) => {
     e.preventDefault()
@@ -91,59 +122,78 @@ export default function MovieReviewMain({
     handleCloseModal()
   }
 
-  console.log(reviewData)
-
   return (
     <>
       <div>
         {movieData.data &&
           movieData.data.map((movie: MovieDataItem) => (
-            <div
-              key={movie.id}
-              className="mx-auto mb-2 flex w-full flex-row justify-between border-y-1 border-gray-600"
-            >
-              <div className="flex h-96 w-[40%] justify-center bg-black p-5">
-                {movie.poster_path ? (
-                  <img
-                    src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`}
-                    alt={movie.title}
-                    className="rounded-2xl"
-                  />
-                ) : (
-                  <span className="text-xl text-stone-500">No Poster</span>
-                )}
-              </div>
-              <div className="flex h-96 w-[60%] flex-col justify-between bg-neutral-300 p-5">
-                <div className="flex flex-col">
-                  <div className="pb-2 text-left text-xl font-bold underline">
-                    {movie.title}
+            <div className="mb-2">
+              <div>
+                <div
+                  key={movie.id}
+                  className="mx-auto flex w-full flex-row justify-between border-y-1 border-gray-600"
+                >
+                  <div className="flex h-96 w-[40%] justify-center bg-black p-5">
+                    {movie.poster_path ? (
+                      <img
+                        src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`}
+                        alt={movie.title}
+                        className="rounded-2xl"
+                      />
+                    ) : (
+                      <span className="text-xl text-stone-500">No Poster</span>
+                    )}
                   </div>
-                  <div className="line-clamp-6 text-justify">
-                    {movie.overview}
-                  </div>
-                </div>
+                  <div className="flex h-96 w-[60%] flex-col justify-between bg-neutral-300 p-5">
+                    <div className="flex flex-col">
+                      <div className="pb-2 text-left text-xl font-bold underline">
+                        {movie.title}
+                      </div>
+                      <div className="line-clamp-6 text-justify">
+                        {movie.overview}
+                      </div>
+                    </div>
 
-                <div>
-                  <div className="flex flex-row gap-2">
-                    <button
-                      type="button"
-                      className="flex h-10 w-full cursor-pointer items-center justify-center border-2 border-black bg-white p-2 hover:bg-orange-500 hover:text-white"
-                      onClick={() => handleOpenModal(movie)}
-                    >
-                      Add A Review
-                    </button>
-                    <button
-                      type="button"
-                      className="flex h-10 w-full cursor-pointer items-center justify-center border-2 border-black bg-white p-2 hover:bg-green-700 hover:text-white"
-                    >
-                      Show Reviews
-                    </button>
-                  </div>
-                  <div className="flex flex-row justify-between">
-                    <div className="flex">{movie.vote_average}</div>
-                    <div className="flex">{movie.release_date}</div>
+                    <div>
+                      <div className="flex flex-row gap-2">
+                        <button
+                          type="button"
+                          className="flex h-10 w-full cursor-pointer items-center justify-center border-2 border-black bg-white p-2 hover:bg-orange-500 hover:text-white"
+                          onClick={() => handleOpenModal(movie)}
+                        >
+                          Add A Review
+                        </button>
+                        <button
+                          type="button"
+                          className="flex h-10 w-full cursor-pointer items-center justify-center border-2 border-black bg-white p-2 hover:bg-green-700 hover:text-white"
+                          onClick={() => handleReview(movie)}
+                        >
+                          {`${getReviewCount(reviewData, movie.id)}`}
+                        </button>
+                      </div>
+                      <div className="flex flex-row justify-between">
+                        <div className="flex">{movie.vote_average}</div>
+                        <div className="flex">{movie.release_date}</div>
+                      </div>
+                    </div>
                   </div>
                 </div>
+                {showReview.show &&
+                  showReview.id === movie.id &&
+                  reviewData
+                    .filter((review) => review.movieId === movie.id)
+                    .map((review) => (
+                      <div className="flex w-full flex-col justify-between">
+                        <div className="flex w-full flex-row justify-between border-b-1 border-dashed border-stone-400 bg-neutral-300 p-5">
+                          <div className="w-20 pr-2 text-4xl">
+                            {review.movieRating}
+                          </div>
+                          <div className="flex w-4/5 text-justify">
+                            {review.movieReview}
+                          </div>
+                        </div>
+                      </div>
+                    ))}
               </div>
             </div>
           ))}
